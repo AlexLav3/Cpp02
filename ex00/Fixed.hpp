67e8000b502c1c72501e4abef2ca277833df7a8b
Alex <alexlav.elly@gmail.com>
2025-11-19 18:50:39 +0100

    setup ex00

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <cctype> 
+
+class Fixed
+{
+    private: 
+    int value; 
+    static const int fract_bits; 
+
+    public:
+    Fixed();
+    ~Fixed();
+    Fixed(const Fixed&);
+    Fixed& operator= (const Fixed& Fixed);
+    int getRawBits()const;
+    void setRawBits(int const raw);
+};
