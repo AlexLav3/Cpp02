1194a06fe614be15398998ba92871ce8c868476a
Alex <alexlav.elly@gmail.com>
2025-11-20 03:22:12 +0100

    fix copy constructor

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -1,2 +1,29 @@
 #include "Fixed.hpp"
 
+Fixed::Fixed(){
+    this->value = 0;
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &copy)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    this->value = copy.value;
+}
+
+Fixed& Fixed::operator= (const Fixed& Fixed)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    return *this;
+}
+
+int Fixed::getRawBits() const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return value; 
+}
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -7,12 +7,12 @@ class Fixed
 {
     private: 
     int value; 
-    static const int fract_bits; 
+    static const int fract_bits = 8; 
 
     public:
     Fixed();
     ~Fixed();
-    Fixed(const Fixed&);
+    Fixed(const Fixed &copy);
     Fixed& operator= (const Fixed& Fixed);
     int getRawBits()const;
     void setRawBits(int const raw);
